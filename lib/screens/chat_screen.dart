6c5a3f942fe44b363fc651a875e3fdb0e5b10aca
Daniel Amarante <daniel.amarante2@gmail.com>
2020-07-29 17:58:58 -0300

    Add send button to screen

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -17,7 +17,8 @@ class ChatScreen extends StatelessWidget {
     ChatMessage("like*", ChatMessageType.incoming),
     ChatMessage("yes I do", ChatMessageType.outgoing),
     ChatMessage("what a coincidence, me too!!!", ChatMessageType.incoming)
-  ];
+  ].reversed.toList();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,35 +30,62 @@ class ChatScreen extends StatelessWidget {
                 child: Column(children: <Widget>[
                   Expanded(
                       child: ListView.builder(
+                    reverse: true,
                     itemCount: messages.length,
                     itemBuilder: (BuildContext context, int index) {
                       return _buildMessage(messages[index]);
                     },
                   )),
-                  TextField(
-                      decoration: InputDecoration(hintText: 'Send message')),
+                  _buildSeparator(),
+                  _buildMessageComposer()
                 ]))));
   }
 
   _buildMessage(ChatMessage chatMessage) {
+    final color = chatMessage.type == ChatMessageType.incoming
+        ? Colors.deepOrange
+        : Colors.cyan;
+    final alignment = chatMessage.type == ChatMessageType.incoming
+        ? Alignment.centerLeft
+        : Alignment.centerRight;
     return Column(
       children: <Widget>[
         Align(
-          alignment: chatMessage.type == ChatMessageType.incoming
-              ? Alignment.centerLeft
-              : Alignment.centerRight,
+          alignment: alignment,
           child: Container(
             padding: EdgeInsets.all(10),
             margin: EdgeInsets.all(5),
             decoration: BoxDecoration(
-                color: chatMessage.type == ChatMessageType.incoming
-                    ? Colors.deepOrange
-                    : Colors.cyan,
+                color: color,
                 borderRadius: BorderRadius.all(Radius.circular(10))),
             child: Text(chatMessage.message),
           ),
         ),
       ],
     );
+  }
+
+  _buildSeparator() {
+    return SizedBox(
+      height: 10.0,
+      child: new Center(
+        child: new Container(
+          height: 1.0,
+          color: Colors.yellow,
+        ),
+      ),
+    );
+  }
+
+  _buildMessageComposer() {
+    return Container(
+        child: Row(children: [
+      Expanded(
+          child: TextField(
+              decoration: InputDecoration(
+                  contentPadding: EdgeInsets.all(5),
+                  hintText: 'Send message'))),
+      IconButton(icon: Icon(Icons.send), onPressed: () {})
+    ]));
   }
 }
